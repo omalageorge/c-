3854a482e180c813a5e0c38b6e370890708e80e8
omalageorge <81519961+omalageorge@users.noreply.github.com>
2022-03-13 22:59:14 +0300

    Add files via upload

---
+++ calculating days of the week using c++.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+int day;
+cout << "Enter from 1 to 7"<<endl;
+cin >> day;
+switch (day){
+	case 1:
+		cout << "Today is monday";
+	break;
+		case 2:
+			cout << "Today is tuesday";
+			break;
+			case 3:
+				cout < "Today is wednesday";
+				break;
+				case 4:
+					cout << "Today is thursday";
+					break;
+					case 5:
+						cout << "Today is friday";
+						break;
+						case 6:
+							cout << "Today is saturday";
+							break;
+							case 7:
+								cout << "Today is sunday";
+											
+	
+}
+	
+	
+	return 0;
+}
